bfad4933bfb60b3e99dc096f7a0ccc565fb85a60
Brenden <brenden.niedermeyer@gmail.com>
2019-12-20 11:51:08 -0800

    feat: implement dark sky call

--- src/http/get-forecast/index.js
+++ src/http/get-forecast/index.js
@@ -1,38 +1,29 @@
 // Enable secure sessions, express-style middleware, and more:
 // https://docs.begin.com/en/functions/http/
 //
-// let begin = require('@architect/functions')
-
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
+let begin = require('@architect/functions');
 
 // HTTP function
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8'
-    },
-    body: html
+  if (
+    req.queryStringParameters &&
+    req.queryStringParameters.lat &&
+    req.queryStringParameters.lng
+  ) {
+    const lat = req.queryStringParameters.lat;
+    const lng = req.queryStringParameters.lng;
+    const forecast = await fetch(
+      `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${lat},${lng}`
+    );
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json; charset=utf8' },
+      body: forecast
+    };
+  } else {
+    return {
+      statusCode: 400,
+      headers: { 'Content-Type': 'application/json; charset=utf8' }
+    };
   }
-}
+};
